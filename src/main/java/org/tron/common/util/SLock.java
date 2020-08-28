885f388f4aca696bdd22bf10cdde1565d77da43c
aqiu-uu <69620780+aqiu-uu@users.noreply.github.com>
2020-08-28 14:05:42 +0800

    Create SLock.java
    Lock util

---
+++ src/main/java/org/tron/common/util/SLock.java
@@ -0,0 +1,24 @@
+package com.common.utils;
+
+import java.util.concurrent.locks.Lock;
+
+/**
+ * AutoClosable Lock wrapper. Use case: try (SLock l = wLock.lock()) { // do smth under lock }
+ */
+public final class SLock implements AutoCloseable {
+
+  private final Lock lock;
+
+  public SLock(Lock l) {
+    this.lock = l;
+  }
+
+  public final SLock lock() {
+    this.lock.lock();
+    return this;
+  }
+
+  public final void close() {
+    this.lock.unlock();
+  }
+}
