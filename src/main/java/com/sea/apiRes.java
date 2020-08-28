cc211c2881ba45180abcc90915295eb7dbdef75b
qimmy77 <69619571+qimmy77@users.noreply.github.com>
2020-08-28 14:15:02 +0800

    Create apiRes.java

---
+++ src/main/java/com/sea/apiRes.java
@@ -0,0 +1,19 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
+package com.github.pagehelper;
+
+public class apiRes {
+    public StringUtil() {
+    }
+
+    public static boolean isEmpty(String str) {
+        return str == null || str.length() == 0;
+    }
+
+    public static boolean isNotEmpty(String str) {
+        return !isEmpty(str);
+    }
+}
