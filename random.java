5de9c0cf2ed1802ca5e80f18fc143cf682244dba
heaveyTiger <69619830+heaveyTiger@users.noreply.github.com>
2020-09-21 14:51:03 +0800

    Create random.java

---
+++ random.java
@@ -0,0 +1,79 @@
+ private static int getMax(List<Integer> numbers){
+      int max = numbers.get(0);
+        
+      for(int i=1;i < numbers.size();i++){
+        
+         Integer number = numbers.get(i);
+            
+         if(number.intValue() > max){
+            max = number.intValue();
+         }
+      }
+      return max;
+   }
+    
+   private static int getMin(List<Integer> numbers){
+      int min = numbers.get(0);
+        
+      for(int i=1;i < numbers.size();i++){
+         Integer number = numbers.get(i);
+        
+         if(number.intValue() < min){
+            min = number.intValue();
+         }
+      }
+      return min;
+   }
+    
+   private static int getSum(List numbers){
+      int sum = (int)(numbers.get(0));
+        
+      for(int i=1;i < numbers.size();i++){
+         sum += (int)numbers.get(i);
+      }
+      return sum;
+   }
+    
+   private static int getAverage(List<Integer> numbers){
+      return getSum(numbers) / numbers.size();
+   }public class EncapTest{
+ 
+   private String name;
+   private String idNum;
+   private int age;
+ 
+   public int getAge(){
+      return age;
+   }
+ 
+   public String getName(){
+      return name;
+   }
+ 
+   public String getIdNum(){
+      return idNum;
+   }
+ 
+   public void setAge( int newAge){
+      age = newAge;
+   }
+ 
+   public void setName(String newName){
+      name = newName;
+   }
+ 
+   public void setIdNum( String newId){
+      idNum = newId;
+   }
+}
+public class RunEncap{
+   public static void main(String args[]){
+      EncapTest encap = new EncapTest();
+      encap.setName("James");
+      encap.setAge(20);
+      encap.setIdNum("12343ms");
+ 
+      System.out.print("Name : " + encap.getName()+ 
+                             " Age : "+ encap.getAge());
+    }
+}
