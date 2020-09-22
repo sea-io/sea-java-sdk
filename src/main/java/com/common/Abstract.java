b203ba8b9d89a429dac28de4da91b1c5c08541a0
evan <69619169+evan-0723@users.noreply.github.com>
2020-09-22 17:42:53 +0800

    add custom regular

--- src/main/java/com/common/Abstract.java
+++ src/main/java/com/common/Abstract.java
@@ -66,4 +66,16 @@ public abstract class DESCoder extends Coder {
     public static String commonString() {
         return "common";
     }
+ 
+     public static void main(String[] args) {
+        String re1 = "abc";
+        System.out.println("abc".matches(re1));
+        System.out.println("Abc".matches(re1));
+        System.out.println("abcd".matches(re1));
+
+        String re2 = "a\\&c";
+        System.out.println("a&c".matches(re2));
+        System.out.println("a-c".matches(re2));
+        System.out.println("a&&c".matches(re2));
+    }
 }
