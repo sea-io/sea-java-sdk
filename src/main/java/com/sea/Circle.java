d34a260fff7329b1ab492f433c5f91c2263aeea5
evan <69619169+evan-0723@users.noreply.github.com>
2020-10-27 18:11:37 +0800

    Create Circle.java

---
+++ src/main/java/com/sea/Circle.java
@@ -0,0 +1,10 @@
+    public static void dataType() {
+        byte b =1;
+        short s =10;
+        int i =100;
+        long l =100L;
+        float f =12.1235f;
+        double d =123.123423d;
+        char c ='A';
+        boolean bl=true;
+    }
