d1ed419dec9eeea758e05a1a0cb303dac8835748
evan <69619169+evan-0723@users.noreply.github.com>
2020-10-27 18:12:54 +0800

    Create Bookmarked.java

---
+++ src/main/java/com/sea/Bookmarked.java
@@ -0,0 +1,16 @@
+    public static void Math() {
+        int a =10;
+        int b =2;
+        System.out.println(a/b);
+        System.out.println(a%b);
+        System.out.println(a*b);
+        System.out.println(a-b);
+        System.out.println(a+b);
+    }
+
+    public static void boolType() {
+        int a=10,b=11;
+        System.out.println(a>9&&b<12); //true 
+        System.out.println(a<10||b>10); // true
+        System.out.println(!(a>0));// false
+    }
