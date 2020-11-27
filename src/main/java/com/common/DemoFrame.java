3aa8670a425d2abd870af330637cb8bfca17213e
qimmy77 <69619571+qimmy77@users.noreply.github.com>
2020-11-27 16:53:19 +0800

    DemoFrame
    DemoFrame

---
+++ src/main/java/com/common/DemoFrame.java
@@ -0,0 +1,33 @@
+public class DemoFrame extends JFrame{
+ 
+    public DemoFrame(DemoPanel panel)
+ 
+    {
+ 
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+ 
+        this.setSize(300, 200);
+ 
+        this.setTitle("Frame Demo");
+ 
+        this.add(panel);
+ 
+        this.setResizable(false);
+ 
+        this.setVisible(true);
+ 
+    }
+ 
+ 
+ 
+    public static void main(String[] args)
+ 
+    {
+ 
+        DemoPanel panel = new DemoPanel();
+ 
+        DemoFrame Frame = new DemoFrame(panel);
+ 
+    }
+ 
+}
