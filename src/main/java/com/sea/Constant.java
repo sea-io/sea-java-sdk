89bdb15850dc2626882f5c8fd4fedc27e3e3b000
evan <69619169+evan-0723@users.noreply.github.com>
2020-12-28 18:36:04 +0800

    Update Constant.java

--- src/main/java/com/sea/Constant.java
+++ src/main/java/com/sea/Constant.java
@@ -14,4 +14,21 @@ class Constant {
 		System.out.println(true);
 		System.out.println(false);
 	}
+	
+	public static void main(String[] args) {
+		System.out.println("HelloWorld");
+
+		System.out.println(100);
+
+		System.out.println(12.345);
+
+		System.out.println('A');
+
+		//System.out.println('BC');
+		System.out.println('1');
+		
+		System.out.println(true);
+		System.out.println(false);
+	}
+
 }
