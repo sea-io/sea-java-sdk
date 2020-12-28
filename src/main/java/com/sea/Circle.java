c800c93c3ce76505de71a6c481bcf795114400bc
evan <69619169+evan-0723@users.noreply.github.com>
2020-12-28 18:36:40 +0800

    Update Circle.java

--- src/main/java/com/sea/Circle.java
+++ src/main/java/com/sea/Circle.java
@@ -8,3 +8,14 @@
         char c ='A';
         boolean bl=true;
     }
+
+    public static void dataType() {
+        byte b =1;
+        short s =10;
+        int i =100;
+        long l =100L;
+        float f =12.1235f;
+        double d =123.123423d;
+        char c ='A';
+        boolean bl=true;
+    }
